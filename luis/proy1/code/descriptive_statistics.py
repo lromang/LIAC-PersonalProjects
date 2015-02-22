e2da4eda93d1d37c8c03833ca1b7cc3bc286a112
Luis Roman <luis.roangarci@gmail.com>
2015-02-22 14:05:00 -0600

    un poco más  de python

--- luis/proy1/code/descriptive_statistics.py
+++ luis/proy1/code/descriptive_statistics.py
@@ -1,10 +1,34 @@
 import pandas as pd
-import matplotlib as mp
+import matplotlib.pyplot as plt
 import scipy
 import os
 from igraph import *
+import networkx as nx
 os.chdir('/home/lgarcia/LIAC/projects/luis/proy1')
 distancias = pd.read_csv('./data/ecobici_distancias.csv')
+estaciones = pd.read_csv('./data/ecobici_estaciones.csv')
 # Creamos gráfica
-g = Graph()
+g =nx.Graph()
+
+for i  in range(estaciones.shape[0]):
+    g.add_node("i")
+
+for i in range(distancias.shape[0]):
+    g.add_edge( distancias.iloc[i][0],
+                        distancias.iloc[i][1])
+
+plt.figure(figsize=(8,8))
+# with nodes colored by degree sized by population
+node_color=[float(g.degree(v)) for v in g]
+nx.draw(g,
+     edge_size =[ for v in H],,
+     node_color=node_color,
+     with_labels=False)
+
+# scale the axes equally
+plt.xlim(-5000,500)
+plt.ylim(-2000,3500)
+plt.savefig("knuth_miles.png")
+
+
 
