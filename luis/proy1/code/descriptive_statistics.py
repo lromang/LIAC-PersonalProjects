fc1159fbb6e88f758e83337e084823255b13c5d5
Luis Roman <luis.roangarci@gmail.com>
2015-02-22 11:25:40 -0600

    algo de python

--- luis/proy1/code/descriptive_statistics.py
+++ luis/proy1/code/descriptive_statistics.py
@@ -1,4 +1,10 @@
-import pandas
-import matplotlib
+import pandas as pd
+import matplotlib as mp
 import scipy
-import igraph
+import os
+from igraph import *
+os.chdir('/home/lgarcia/LIAC/projects/luis/proy1')
+distancias = pd.read_csv('./data/ecobici_distancias.csv')
+# Creamos gráfica
+g = Graph()
+
